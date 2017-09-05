7de293468f2b9f9b3812abb02be140c6fcee6dcf
Samir Gossain <samir.gossain@gmail.com>
2017-09-05 16:02:49 +0100

    Saving Progress

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @question = Question.create(script: 'test')
+  end
+
+  it 'Question Text is properly saved' do
+    expect(@question.script).to eq('test')
+  end
+
+  it 'Question Text can be changed' do
+    params = { script: 'test2'}
+    @question.update(params)
+    expect(@question.script).to eq('test2')
+  end
 end
