c477c5f32b580d9b7c5ed123590ed2ecb9de973e
Samir Gossain <samir.gossain@gmail.com>
2017-09-06 09:54:59 +0100

    Adds Success Screen and dbseeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Rounds
+Round.create(number: 1)
+Round.create(number: 2)
+Round.create(number: 3)
+Round.create(number: 4)
+Round.create(number: 5)
+
+#Questions
+Question.create(script: 'Russia has a larger surface area than...')
+Question.create(script: 'Nintendo was originally a...')
+Question.create(script: 'On Jupiter and Saturn it rains...')
+Question.create(script: 'You are more likely to be killed by a ____ than a Shark.')
+Question.create(script: 'Scotlands National Animal is a...')
+
+#Messages
+Message.create(script: 'Pluto')
+Message.create(script: 'The Indian Ocean')
+Message.create(script: 'Trading Card Company')
+Message.create(script: 'Family Restaurant')
+Message.create(script: 'Diamonds')
+Message.create(script: 'Fire')
+Message.create(script: 'Vending Machine')
+Message.create(script: 'Conker')
+Message.create(script: 'Unicorn')
+Message.create(script: 'Traditional Horse')
